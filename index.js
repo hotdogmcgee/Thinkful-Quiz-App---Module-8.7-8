9fbce3e4928baf78afbd57677ebf62701c0544b4
Kevin Robinson <kevinrobinsondeveloper@gmail.com>
2019-03-05 12:51:15 -0500

    minor changes to js file

--- index.js
+++ index.js
@@ -18,38 +18,59 @@ let questionIndex = 0;
 
 
 function handleSubmit() {
-    $('main').on('click', '.submit-button', function(event) {
+    $('form').on('submit', function(event) {
         event.preventDefault();
-        const answer = $('input:checked').val();
-        const userIsCorrect = checkAnswer(answer);
-        if(userIsCorrect) {
-            showCorrect();
+        let answer = $('input:checked');
+        let answerVal = answer.val();
+        let correctAnswer = `${STORE[questionIndex].correctAnswer}`
+        if (answerVal === correctAnswer) {
+            renderMain(true, false);
             } else {
-            showIncorrect();
+            renderMain(false, false);
             }
         console.log('yoyoy');
     })
 }
 
-// function handleSubmitButton() {
-//     $('#container').on('click', '#js-submit-button', function(event) {
-//       event.preventDefault()
-  
-//       const answer = $('input:checked').siblings('span');
-  
-//       const userIsCorrect = checkUserAnswer(answer);
-//       if(userIsCorrect) {
-//         generateCorrectFeedback();
+//user selects answer on submit run user feedback
+// function userSelectAnswer () {
+//     $('form').on('submit', function (event) {
+//       event.preventDefault();
+//       let selected = $('input:checked');
+//       let answer = selected.val();
+//       let correctAnswer = `${STORE[questionNumber].correctAnswer}`;
+//       if (answer === correctAnswer) {
+//         selected.parent().addClass('correct');
+//         ifAnswerIsCorrect();
 //       } else {
-//         generateIncorrectFeedback();
+//         selected.parent().addClass('wrong');
+//         ifAnswerIsWrong();
 //       }
 //     });
 //   }
+
 //returns HTML in DOM when user clicks on next question/start quiz
 function createQuestion() {
-    $('main').html(` <section id="question-page" role="main"><h2 id="question">${STORE[questionIndex].question}</h2><form><fieldset class="quiz-form">
-<label class="answer-template"><input class="answer" type="radio" name="option" checked=""><span>${STORE[questionIndex].answers[0]}</span></label><label class="answer-template"> <input class="answer" type="radio" name="option" checked=""><span>${STORE[questionIndex].answers[1]}</span></label><label class="answer-template"><input class="answer" type="radio" name="option" checked=""><span>${STORE[questionIndex].answers[2]}</span></label><label class="answer-template"><input class="answer" type="radio" name="option" checked=""><span>${STORE[questionIndex].answers[3]}</span></label></fieldset></form></section>
-<button class="submit-button js-next-question">Submit</button>`)
+    $('main').html(` <section id="question-page" role="main">
+        <h2 id="question">${STORE[questionIndex].question}</h2>
+        <form>
+            <fieldset class="quiz-form">
+                <label class="answer-template">
+                    <input class="answer" type="radio" name="option" checked=""><span>${STORE[questionIndex].answers[0]}</span>
+                </label>
+                <label class="answer-template"> 
+                    <input class="answer" type="radio" name="option" checked=""><span>${STORE[questionIndex].answers[1]}</span>
+                    </label>
+                <label class="answer-template">
+                    <input class="answer" type="radio" name="option" checked=""><span>${STORE[questionIndex].answers[2]}</span>
+                </label>
+                <label class="answer-template">
+                    <input class="answer" type="radio" name="option" checked=""><span>${STORE[questionIndex].answers[3]}</span>
+                </label>
+                <button class="submit-button js-next-question">Submit</button>
+            </fieldset>
+        </form>
+        </section>`)
 }
 
 //larger function designed to handle rendering of Main section when form button is clicked, calls on other functions to return values
@@ -108,15 +129,15 @@ function handleRestart() {
 }
 
 //checks if user submitted correct answer
-function checkAnswer(var1) {
-    if (var1 === STORE[questionIndex].correctAnswer) {
-        console.log('hello');
-        return true
-    } else {
-        console.log('goodbye')
-        return false
-    }
-}
+// function checkAnswer(var1) {
+//     if (var1 === STORE[questionIndex].correctAnswer) {
+//         console.log('hello');
+//         return true
+//     } else {
+//         console.log('goodbye')
+//         return false
+//     }
+// }
 
 //adds to user score when a correct answer is submitted
 function addToScore () {
@@ -140,12 +161,4 @@ function makeQuiz() {
 }
 
 $(makeQuiz);
-
-
-// function checkUserAnswer(answer) {
-//     if(answer.text() === ANSWERS[questionNum - 1]) {
-//       return true;
-//     } else {
-//       return false;
-//     }
 
