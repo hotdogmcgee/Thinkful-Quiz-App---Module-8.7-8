1d2516ccd6ca95531eb25013f6d00fcfde102642
Kevin Robinson <kevinrobinsondeveloper@gmail.com>
2019-03-05 12:24:52 -0500

    added more JS functionality, working on Submit button

--- index.js
+++ index.js
@@ -16,7 +16,36 @@ let questionNum = 1;
 let questionIndex = 0;
 
 
-//returns HTML in DOM when user clicks on next question
+
+function handleSubmit() {
+    $('main').on('click', '.submit-button', function(event) {
+        event.preventDefault();
+        const answer = $('input:checked').val();
+        const userIsCorrect = checkAnswer(answer);
+        if(userIsCorrect) {
+            showCorrect();
+            } else {
+            showIncorrect();
+            }
+        console.log('yoyoy');
+    })
+}
+
+// function handleSubmitButton() {
+//     $('#container').on('click', '#js-submit-button', function(event) {
+//       event.preventDefault()
+  
+//       const answer = $('input:checked').siblings('span');
+  
+//       const userIsCorrect = checkUserAnswer(answer);
+//       if(userIsCorrect) {
+//         generateCorrectFeedback();
+//       } else {
+//         generateIncorrectFeedback();
+//       }
+//     });
+//   }
+//returns HTML in DOM when user clicks on next question/start quiz
 function createQuestion() {
     $('main').html(` <section id="question-page" role="main"><h2 id="question">${STORE[questionIndex].question}</h2><form><fieldset class="quiz-form">
 <label class="answer-template"><input class="answer" type="radio" name="option" checked=""><span>${STORE[questionIndex].answers[0]}</span></label><label class="answer-template"> <input class="answer" type="radio" name="option" checked=""><span>${STORE[questionIndex].answers[1]}</span></label><label class="answer-template"><input class="answer" type="radio" name="option" checked=""><span>${STORE[questionIndex].answers[2]}</span></label><label class="answer-template"><input class="answer" type="radio" name="option" checked=""><span>${STORE[questionIndex].answers[3]}</span></label></fieldset></form></section>
@@ -27,35 +56,50 @@ function createQuestion() {
 function renderMain(isCorrect, renderNext) {
     if (renderNext === true){
         createQuestion();
-        questionIndex++;
+        increaseQuestionIndex();
         increaseQuestionNumber();
         console.log('second')
       }
       else {
             if (isCorrect === true) {
-           //render correct html
-           addToScore();
+                showCorrect();
+                addToScore();
+                increaseQuestionIndex();
+                increaseQuestionNumber();
             }
              else {
-            //render wrong html
+                showIncorrect();
+                increaseQuestionIndex();
+                increaseQuestionNumber();
             }
       }
 }
 
 //returns a page showing that a user clicked the correct answer
 function showCorrect() {
-
+    console.log('correct answer submitted');
+    $('main').html(`
+    <section class="feedback-page" role="main">
+        <h2>Correct!</h2>
+        <img src="https://github.com/hotdogmcgee/Thinkful-Quiz-App---Module-8.7-8/blob/master/images/Segovia%20-%20You%20Got%20it%20Right!.jpg?raw=true" alt="Segovia Smiling">
+        <button id="js-next-button">Next Question</button>
+    </section>
+    `)
 }
 
 //returns a page showing that a user clicked the wrong answer, provides feedback.
 function showIncorrect() {
-
+    console.log('wrong answer submitted');
 }
 
 //handles event for when user starts a quiz
 function handleStart() {
-    $('.js-next-question').on('click', renderMain(false, true))
-    console.log('first')
+    console.log('first');
+    $('.js-first-question').on('click', function(event){
+        renderMain(false, true);
+        console.log('four')
+    })
+    console.log('third');
 }
 
 //handles event for when user re-starts a quiz
@@ -63,21 +107,45 @@ function handleRestart() {
 
 }
 
+//checks if user submitted correct answer
+function checkAnswer(var1) {
+    if (var1 === STORE[questionIndex].correctAnswer) {
+        console.log('hello');
+        return true
+    } else {
+        console.log('goodbye')
+        return false
+    }
+}
+
 //adds to user score when a correct answer is submitted
 function addToScore () {
-    userScore++
+    userScore++;
 }
 
-//increases question number, to be used when 'next question is clicked.
+//increases question number, to be used when 'next question' is clicked.
 function increaseQuestionNumber () {
-    questionNum++
+    questionNum++;
 }
 
+//increases the variable questionIndex, which is used for the STORE array in data.js
+function increaseQuestionIndex() {
+    questionIndex++;
+}
 
 
 function makeQuiz() {
     handleStart();
-
+    handleSubmit();
 }
 
 $(makeQuiz);
+
+
+// function checkUserAnswer(answer) {
+//     if(answer.text() === ANSWERS[questionNum - 1]) {
+//       return true;
+//     } else {
+//       return false;
+//     }
+
